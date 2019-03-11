84cb717761ccfa89645153b6e2d3985697761672
OriginalEXE <antesepic@gmail.com>
2019-03-11 17:05:30 +0100

    :recycle: Convert to v-model, update the way translations are handled

--- src/stories/HdInput.stories.js
+++ src/stories/HdInput.stories.js
@@ -9,21 +9,142 @@ storiesOf('Form/HdInput', module)
   .addDecorator(FormWrapper)
   .add('required', () => ({
     components: { HdInput },
-    template: '<hd-input @dataChange="onDataChange" :required="true" label="Label" placeholder="Placeholder..." />',
-    methods: { onDataChange: action('dataChange') },
+    template: `
+      <hd-input
+        v-model="value"
+        :required="true"
+        name="test"
+        label="Label"
+        placeholder="Placeholder..."
+      />
+    `,
+    data() {
+      return {
+        value: '',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
   }))
   .add('prefilled', () => ({
-    components: { HdInput },
-    template: '<hd-input @dataChange="onDataChange" value="Your default value goes here" label="Label..." placeholder="Placeholder..." />',
     methods: { onDataChange: action('dataChange') },
+    components: { HdInput },
+    template: `
+      <hd-input
+        v-model="value"
+        name="test"
+        label="Label"
+        placeholder="Placeholder..."
+      />
+    `,
+    data() {
+      return {
+        value: 'Your default value goes here',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
   }))
   .add('password', () => ({
     components: { HdInput },
-    template: '<hd-input @dataChange="onDataChange" type="password" :required="true" label="Password" />',
-    methods: { onDataChange: action('dataChange') },
+    template: `
+      <hd-input
+        v-model="value"
+        name="password"
+        label="Password"
+        type="password"
+      />
+    `,
+    data() {
+      return {
+        value: '',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
   }))
   .add('email', () => ({
     components: { HdInput },
-    template: '<hd-input @dataChange="onDataChange" type="email" label="Email" placeholder="Enter your email address" />',
-    methods: { onDataChange: action('dataChange') },
+    template: `
+      <hd-input
+        v-model="value"
+        name="email"
+        label="Email"
+        placeholder="jane.doe@homeday.de"
+        type="email"
+      />
+    `,
+    data() {
+      return {
+        value: '',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
+  }))
+  .add('number', () => ({
+    components: { HdInput },
+    template: `
+      <hd-input
+        v-model="value"
+        :min="0"
+        :max="99"
+        name="number"
+        label="Number"
+        placeholder=">= 0, < 100"
+        type="number"
+      />
+    `,
+    data() {
+      return {
+        value: '',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
+  }))
+  .add('custom translation', () => ({
+    components: { HdInput },
+    template: `
+      <hd-input
+        v-model="value"
+        :required="true"
+        :texts="texts"
+        name="test"
+        label="Label"
+        placeholder="Placeholder..."
+      />
+    `,
+    data() {
+      return {
+        value: '',
+        texts: {
+          FORM: {
+            VALIDATION: {
+              REQUIRED: 'Hey you, yeah you! Fill it out!',
+            },
+          },
+        },
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
   }));
