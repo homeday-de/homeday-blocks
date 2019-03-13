6f1f7b2f63d024c4634f2190bf6d8b22e6039104
Ilyas Amezouar <ilyas@amezouar.com>
2019-03-13 15:53:42 +0100

    :sparkle: The FormWrapper now supports knobs

--- src/storiesWrappers/FormWrapper.js
+++ src/storiesWrappers/FormWrapper.js
@@ -1,5 +1,12 @@
-export default function FormWrapper() {
+// "Hacky", but this way we can support knobs with other decorators, (Storybook's docs for Vue suck!)
+export default (story) => {
+  const storyComp = story();
   return {
-    template: '<div style="margin: 64px auto; max-width: 360px;"><story/></div>',
+    ...storyComp,
+    template: `
+      <div style="margin: 64px auto; max-width: 360px;">
+        ${storyComp.template}
+      </div>
+    `,
   };
-}
+};
