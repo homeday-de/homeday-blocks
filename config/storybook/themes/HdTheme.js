5bf35bbd3c843e82e7b86f0973462326ebfe4261
Ilyas Amezouar <ilyas@amezouar.com>
2019-03-14 20:30:48 +0100

    :lipstick: Customized the theme

---
+++ config/storybook/themes/HdTheme.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { create } from '@storybook/theming';
+import logo from './logo.svg';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Homeday',
+  brandUrl: 'https://www.homeday.de',
+  brandImage: logo,
+});
