647904cd1ff4199556442d0ab4e6a2516087d6c2
Daniele Rapisarda <info@danielerapisarda.com>
2019-03-15 10:37:59 +0100

    :white_check_mark: HdTimeslots tests

--- tests/unit/components/tooltip/HdTooltip.spec.js
+++ tests/unit/components/tooltip/HdTooltip.spec.js
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-import { mount } from '@vue/test-utils';
-=======
 import { shallowMount } from '@vue/test-utils';
->>>>>>> :white_check_mark: HDTooltip tests
 import HdTooltip from '@/components/tooltip/HdTooltip.vue';
 
 describe('HdTooltip', () => {
   let wrapper;
 
   beforeEach(() => {
-<<<<<<< HEAD
-    wrapper = mount(HdTooltip);
-  });
-
-  test('renders component', () => {
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  test('renders a span', () => {
-    const span = wrapper.find('span');
-
-    expect(span.exists()).toBe(true);
-=======
     wrapper = shallowMount(HdTooltip);
   });
 
@@ -31,6 +14,5 @@ describe('HdTooltip', () => {
 
   test('the window tooltip element is set properly', () => {
     expect(window.tooltipElement).toEqual(wrapper.element);
->>>>>>> :white_check_mark: HDTooltip tests
   });
 });
