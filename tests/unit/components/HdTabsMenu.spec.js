bb0946857fd87b23bba9959d4e9202c179d3b2dc
OriginalEXE <antesepic@gmail.com>
2019-05-23 09:27:53 +0200

    :recycle: Refactor the tests, add props tests

--- tests/unit/components/HdTabsMenu.spec.js
+++ tests/unit/components/HdTabsMenu.spec.js
@@ -12,43 +12,70 @@ describe('HdTabsMenu', () => {
 
   beforeEach(() => {
     wrapper = mount(HdTabsMenu, {
-      // this is needed to trigger window events
-      // when mounting to document, we need to call wrapper.destroy after
-      // every test - https://vue-test-utils.vuejs.org/api/options.html#attachtodocument
-      attachToDocument: true,
+      propsData: {
+        items: ITEMS,
+      },
     });
   });
 
   test('The component is rendered', () => {
-    wrapper.setProps({ items: ITEMS, value: ITEMS[1].value });
+    wrapper.setProps({ value: ITEMS[1].value });
     expect(wrapper.html()).toMatchSnapshot();
     wrapper.destroy();
   });
 
   test('Rendered selected menu item should match passed in preselected item', () => {
-    wrapper.setProps({ items: ITEMS, value: ITEMS[1].value });
+    wrapper.setProps({ value: ITEMS[1].value });
     expect(wrapper.findAll(ACTIVE_MENU_ITEM_SELECTOR).length).toBe(1);
     wrapper.destroy();
   });
 
   test('By default, no menu item is selected', () => {
-    wrapper.setProps({ items: ITEMS });
     expect(wrapper.findAll(ACTIVE_MENU_ITEM_SELECTOR).length).toBe(0);
     wrapper.destroy();
   });
 
   test('Menu item can be selected', () => {
-    wrapper.setProps({ items: ITEMS });
     // select a menu item
     wrapper.find(MENU_ITEM_SELECTOR).trigger('click');
     // confirm that input event is emitted
     expect(wrapper.emitted('input')).toBeTruthy();
     // And that expected payload is passed with the event
     const payload = wrapper.emitted('input')[0][0];
-    expect(payload).toBeTruthy();
+    expect(payload).toBe(ITEMS[0].value);
     // check that a menu item is selected
     wrapper.setProps({ value: payload });
     expect(wrapper.findAll(ACTIVE_MENU_ITEM_SELECTOR).length).toBe(1);
     wrapper.destroy();
+  });
+
+  describe('props', () => {
+    describe('items', () => {
+      test('only accepts Array', () => {
+        const { items } = wrapper.vm.$options.props;
+
+        expect(items.type).toBe(Array);
+      });
+
+      test('defaults to empty array', () => {
+        const { items } = wrapper.vm.$options.props;
+
+        expect(items.default().length).toBe(0);
+      });
+    });
+
+    describe('value', () => {
+      test('only accepts String', () => {
+        const { value } = wrapper.vm.$options.props;
+
+        expect(value.type).toBe(String);
+      });
+
+      test('defaults to empty string', () => {
+        const { value } = wrapper.props();
+
+        expect(value).toBe('');
+      });
+    });
   });
 });
