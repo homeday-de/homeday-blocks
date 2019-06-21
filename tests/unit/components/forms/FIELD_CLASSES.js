04ec947fcb04cd70842a26dd4dee8f31e18ff37f
Ilyas Amezouar <ilyas@amezouar.com>
2019-06-21 16:28:16 +0200

    :truck: Moved HdSelect spec to `form` directory

--- tests/unit/components/forms/FIELD_CLASSES.js
+++
@@ -1,8 +0,0 @@
-export default {
-  ACTIVE: 'field--active',
-  FILLED: 'field--filled',
-  VALID: 'field--valid',
-  INVALID: 'field--invalid',
-  NO_LABEL: 'field--no-label',
-  NO_CONTROL: 'field--hasControl',
-};
