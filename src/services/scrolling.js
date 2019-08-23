a2e07aea0d6c5c391a3620e09389c8c0078036a7
ilyasmez <ilyas@amezouar.com>
2019-08-23 14:33:15 +0200

    [F] Add the new gallery components (#104)
    * :sparkles: Added the new gallery components
    
    * :bug: Fixed the action in HdGalleryTiles' story
    
    * :sparkle: Added stories for HdLazyImage
    
    * :art: Put the gallery stories in a category
    
    * 🔇 Removed the console log

---
+++ src/services/scrolling.js
@@ -0,0 +1,97 @@
+import naturalScroll from 'natural-scroll';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+} from 'body-scroll-lock';
+
+export function elOffset(el) {
+  const box = el.getBoundingClientRect();
+  const clientTop = document.clientTop || document.body.clientTop || 0;
+  const clientLeft = document.clientLeft || document.body.clientLeft || 0;
+  const scrollTop = window.pageYOffset || document.scrollTop || 0;
+  const scrollLeft = window.pageXOffset || document.scrollLeft || 0;
+
+  return {
+    top: box.top + (scrollTop - clientTop),
+    left: box.left + (scrollLeft - clientLeft),
+    width: box.width,
+    height: box.height,
+  };
+}
+
+export function getDocumentHeight() {
+  return Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight,
+  );
+}
+
+export function isWindowFullyScrolled() {
+  if (window.pageYOffset + document.documentElement.clientHeight >= getDocumentHeight()) {
+    return true;
+  }
+
+  return false;
+}
+
+export function freezeScrolling(el) {
+  // eslint-disable-next-line no-param-reassign
+  el.style['-webkit-overflow-scrolling'] = 'touch';
+  disableBodyScroll(el);
+}
+
+export function unfreezeScrolling(el) {
+  // eslint-disable-next-line no-param-reassign
+  el.style['-webkit-overflow-scrolling'] = 'auto';
+  enableBodyScroll(el);
+}
+
+export function isElVisible({ el, offset = 0 }) {
+  if (el == null) {
+    return false;
+  }
+
+  const { top: offsetTop } = elOffset(el);
+
+  if (offsetTop < window.pageYOffset + offset) {
+    return false;
+  }
+
+  if (offsetTop > window.pageYOffset + window.innerHeight) {
+    return false;
+  }
+
+  return true;
+}
+
+export function isScrolledPastElBottom({ el }) {
+  if (el == null) {
+    return false;
+  }
+
+  const { top: offsetTop } = elOffset(el);
+
+  const elBottom = offsetTop + el.clientHeight;
+
+  if (elBottom > window.pageYOffset + window.innerHeight) {
+    return false;
+  }
+
+  return true;
+}
+
+export function scrollToEl({ el, offset = 0, onlyIfNotVisible = true }) {
+  const toScroll = [
+    document.documentElement,
+    document.body,
+  ];
+
+  if (el == null || (onlyIfNotVisible && isElVisible({ el, offset }))) {
+    return;
+  }
+
+  toScroll.forEach((toScrollEl) => {
+    naturalScroll.scrollTop(toScrollEl, elOffset(el).top - offset);
+  });
+}
---
+++ src/stories/HdGalleryTiles.stories.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
+import HdGalleryTiles from 'hd-blocks/components/gallery/HdGalleryTiles.vue';
+import ITEMS from './mocks/GALLERY_ITEMS';
+
+storiesOf('Gallery/HdGalleryTiles', module)
+  .add('default', () => ({
+    components: { HdGalleryTiles },
+    template: `
+      <HdGalleryTiles
+        :items="items"
+        class="expose__gallery expose__gallery--tiles"
+        @itemClick="onItemClick"
+      />
+    `,
+    data() {
+      return {
+        items: ITEMS,
+        visible: false,
+      };
+    },
+    methods: {
+      onItemClick(itemIndex) {
+        action('itemClicked')(itemIndex);
+      },
+    },
+  }));
