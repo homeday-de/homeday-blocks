c65cb18ee259690f9f9493029f751f548f98627b
Daniele Rapisarda <info@danielerapisarda.com>
2019-09-02 15:22:37 +0200

    :lipstick: :bug: style fixes on HdGalleryTiles

--- src/stories/HdGalleryTiles.stories.js
+++ src/stories/HdGalleryTiles.stories.js
@@ -3,18 +3,20 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 
 import HdGalleryTiles from 'hd-blocks/components/gallery/HdGalleryTiles.vue';
+import SizedContainerWrapper from 'hd-blocks/storiesWrappers/SizedContainerWrapper';
 import ITEMS from './mocks/GALLERY_ITEMS';
 
 storiesOf('Gallery/HdGalleryTiles', module)
+  .addDecorator(SizedContainerWrapper)
   .add('default', () => ({
     components: { HdGalleryTiles },
     template: `
-      <HdGalleryTiles
-        :items="items"
-        class="expose__gallery expose__gallery--tiles"
-        @itemClick="onItemClick"
-      />
-    `,
+    <HdGalleryTiles
+      :items="items"
+      class="expose__gallery expose__gallery--tiles"
+      @itemClick="onItemClick"
+    />
+  `,
     data() {
       return {
         items: ITEMS,
