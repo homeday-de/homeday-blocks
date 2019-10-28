06432f199e0b2d5b4668a7660a7a4a835beee6c4
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2019-10-28 13:42:05 +0100

    [F] HdButton (#158)
    * :pencil: Jest watch single component
    
    * :pencil2: Missing new path on example
    
    * :sparkles: Create HdPrimaryButton
    
    * :ok_hand: Create hd-button
    
    * :ok_hand: Use <slot>

---
+++ tests/unit/components/buttons/HdButton.spec.js
@@ -0,0 +1,79 @@
+import { shallowMount } from '@vue/test-utils';
+import HdButton, { TYPES } from '@/components/buttons/HdButton.vue';
+
+describe('HdButton', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(HdButton, {
+      slots: {
+        default: '<span>Button text</span>',
+      },
+    });
+  });
+
+  it('should render component with `btn` class', () => {
+    expect(wrapper.classes()).toContain('btn');
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render component with `btn--primary` class', () => {
+    const modifier = TYPES.PRIMARY;
+    const className = `btn--${modifier}`;
+
+    wrapper.setProps({
+      modifier,
+    });
+
+    expect(wrapper.classes()).toContain(className);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render component with `btn--secondary` class', () => {
+    const modifier = TYPES.SECONDARY;
+    const className = `btn--${modifier}`;
+
+    wrapper.setProps({
+      modifier,
+    });
+
+    expect(wrapper.classes()).toContain(className);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render component with `btn--tertiary` class', () => {
+    const modifier = TYPES.TERTIARY;
+    const className = `btn--${modifier}`;
+
+    wrapper.setProps({
+      modifier,
+    });
+
+    expect(wrapper.classes()).toContain(className);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render component with `btn--flat` class', () => {
+    const modifier = TYPES.FLAT;
+    const className = `btn--${modifier}`;
+
+    wrapper.setProps({
+      modifier,
+    });
+
+    expect(wrapper.classes()).toContain(className);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render component with `btn--ghost` class', () => {
+    const modifier = TYPES.GHOST;
+    const className = `btn--${modifier}`;
+
+    wrapper.setProps({
+      modifier,
+    });
+
+    expect(wrapper.classes()).toContain(className);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
