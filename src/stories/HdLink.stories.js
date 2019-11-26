cdb55e255c1d5be686c03b95922b9a0c759f5c25
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2019-11-26 06:26:25 -0300

    :recycle: <HdLink>

--- src/stories/HdLink.stories.js
+++ src/stories/HdLink.stories.js
@@ -1,10 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue';
 import { text, select } from '@storybook/addon-knobs';
-import HdLink from 'hd-blocks/components/HdLink.vue';
+import HdLink, { TYPES } from 'hd-blocks/components/HdLink.vue';
 import HdLinkNote from '../notes/HdLink.md';
 
-storiesOf('HdLink', module)
+const stories = storiesOf('HdLink', module);
+
+TYPES.forEach((modifier) => {
+  const capitalizedModifier = modifier.charAt(0).toUpperCase() + modifier.slice(1);
+
+  stories.add(capitalizedModifier, () => ({
+    components: { HdLink },
+    data() {
+      return { modifier, capitalizedModifier };
+    },
+    template: `
+      <HdLink
+        :modifier=modifier
+        href="https://www.homeday.de"
+      >
+        {{ capitalizedModifier }}
+      </HdLink>
+    `,
+  }));
+});
+
+stories
   .add('Playground 🎛📝', () => ({
     components: { HdLink },
     props: {
@@ -14,7 +35,7 @@ storiesOf('HdLink', module)
       },
       modifier: {
         type: String,
-        default: select('Modifier', ['primary', 'secondary'], 'primary'),
+        default: select('Modifier', TYPES, TYPES[0]),
       },
     },
     template: `
