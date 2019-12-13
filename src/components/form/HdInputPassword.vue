9b15708383496e614d112351740812e178ed2c21
Aymen Bareche <aym3nb@gmail.com>
2019-12-13 09:45:27 +0100

    :sparkles: Extract HdInputPassword from HdInput

---
+++ src/components/form/HdInputPassword.vue
@@ -0,0 +1,236 @@
+<template>
+  <div
+    :class="fieldClasses"
+    class="field field--input"
+  >
+    <img
+      v-if="icon"
+      :src="icon"
+      role="presentation"
+      class="field__icon"
+    >
+    <input
+      :autocomplete="autocomplete"
+      :value="value"
+      :type="currentType"
+      :id="name"
+      :name="name"
+      :placeholder="isActive && placeholder !== undefined ? placeholder : ''"
+      :required="required"
+      :autofocus="autofocus"
+      :disabled="disabled"
+      class="field__input"
+      ref="input"
+      @input="handleInput"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
+    <label
+      v-if="label"
+      :for="name"
+      class="field__label"
+    >
+      {{ label }}
+    </label>
+    <p
+      v-if="error"
+      class="field__error"
+    >
+      {{ error }}
+    </p>
+    <p
+      v-else-if="helper"
+      class="field__error field__error--helper"
+      v-html="helper"
+    />
+    <span v-if="showVisibilityToggle"
+      class="field__visibilityToggle"
+      :class="{'field__visibilityToggle--visible': currentType === 'text'}"
+      @click="togglePasswordVisibility"
+    />
+    <span class="field__border"/>
+  </div>
+</template>
+
+<script>
+import merge from 'lodash/merge';
+import { getMessages } from 'hd-blocks/lang';
+
+export default {
+  name: 'HdInputPassword',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    autocomplete: {
+      type: String,
+      default: 'on',
+    },
+    autofocus: {
+      type: Boolean,
+      default: false,
+    },
+    lang: {
+      type: String,
+      default: 'de',
+    },
+    texts: {
+      type: Object,
+      default: () => ({}),
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      currentType: 'password',
+      isActive: undefined,
+      isValid: undefined,
+      error: null,
+      helper: null,
+    };
+  },
+  computed: {
+    t() {
+      return merge(getMessages(this.lang), this.texts);
+    },
+    isEmpty() {
+      return this.value === null || this.value === undefined || this.value === '';
+    },
+    showVisibilityToggle() {
+      return this.value.length !== 0;
+    },
+    fieldClasses() {
+      return {
+        'field--active': this.isActive,
+        'field--filled': !this.isEmpty,
+        'field--invalid': this.isValid === false,
+        'field--hasControl': this.showVisibilityToggle,
+        'field--disabled': this.disabled,
+        'field--hasIcon': this.icon,
+      };
+    },
+  },
+  watch: {
+    value() {
+      this.validate();
+    },
+    type(type) {
+      this.currentType = type;
+    },
+  },
+  methods: {
+    clearInput() {
+      this.$refs.input.focus();
+      this.hideError();
+      this.$emit('input', '');
+    },
+    handleFocus() {
+      this.isActive = true;
+      this.$emit('focus');
+    },
+    handleBlur() {
+      this.isActive = false;
+      this.validate();
+      this.$emit('blur');
+    },
+    handleInput(e) {
+      const newValue = e.target.value;
+
+      this.$emit('input', newValue);
+    },
+    showError(errorMessage) {
+      this.error = errorMessage;
+      this.isValid = false;
+    },
+    showHelper(message) {
+      this.helper = message;
+    },
+    hideError() {
+      this.isValid = true;
+      this.error = null;
+    },
+    validate() {
+      if (this.required && this.isEmpty) {
+        this.showError(this.t.FORM.VALIDATION.REQUIRED);
+      } else {
+        this.hideError();
+      }
+
+      return !this.error;
+    },
+    togglePasswordVisibility() {
+      this.currentType = this.currentType === 'password' ? 'text' : 'password';
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import 'hd-blocks/styles/inputs.scss';
+
+.field {
+  &__error {
+    width: 100%;
+    text-align: left;
+    &--helper {
+      display: block;
+      color: $regent-gray;
+    }
+  }
+  &__visibilityToggle {
+    position: absolute;
+    right: $inline-s;
+    bottom: $stack-m;
+    background-image: url('~hd-blocks/assets/icons/ic_visibility-on.svg');
+    background-repeat: no-repeat;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+    &:after {
+      display: block;
+      content: '';
+      position: absolute;
+      opacity: 1;
+      top: 2px;
+      left: 3px;
+      width: 24px;
+      height: 0px;
+      border-bottom: 2px solid $regent-gray;
+      border-top: 1px solid $wild-sand;
+      transform-origin: left;
+      transform: rotateZ(45deg) scaleX(1);
+      transition: transform .3s;
+    }
+    &--visible {
+      &:after {
+        transform: rotateZ(45deg) scaleX(0);
+      }
+    }
+  }
+}
+</style>
---
+++ tests/unit/components/form/HdInputPassword.spec.js
@@ -0,0 +1,116 @@
+import { wrapperFactoryBuilder } from 'tests/unit/helpers';
+import FIELD_CLASSES from './FIELD_CLASSES';
+import HdInputPassword from '@/components/form/HdInputPassword.vue';
+
+const ERROR_SELECTOR = '.field__error';
+const HELPER_SELECTOR = '.field__error--helper';
+const ICON_SELECTOR = '.field__icon';
+const VISIBILITY_TOGGLE_SELECTOR = '.field__visibilityToggle';
+
+const PASSWORD_VISIBLE_CLASS = 'field__visibilityToggle--visible';
+
+const TEST_VALUE = 'new value';
+const TEST_PROPS = {
+  name: 'test name',
+  label: 'test label',
+  placeholder: 'test placeholder',
+  value: 'test password',
+};
+
+describe('HdInputPassword', () => {
+  const wrapperFactory = wrapperFactoryBuilder(
+    HdInputPassword,
+    {
+      propsData: TEST_PROPS,
+    },
+  );
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = wrapperFactory();
+  });
+
+  test('is rendered as expected', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('updates the field on change of the prop `value`', () => {
+    wrapper.setProps({ value: TEST_VALUE });
+    expect(wrapper.find('input').element.value).toBe(TEST_VALUE);
+  });
+
+  test('emits `input` event with the right payload', () => {
+    wrapper.find('input').setValue(TEST_VALUE);
+    expect(wrapper.emitted('input')[0][0]).toEqual(TEST_VALUE);
+  });
+
+  test('allows setting a helper', () => {
+    const TEST_HTML = '<b>test</b> test';
+    wrapper.vm.showHelper(TEST_HTML);
+    expect(wrapper.find(HELPER_SELECTOR).element.innerHTML).toEqual(TEST_HTML);
+  });
+
+  test('validates requiredness', () => {
+    wrapper = wrapperFactory({
+      propsData: {
+        value: '',
+        required: true,
+      },
+    });
+    expect(wrapper.vm.validate()).toBe(false);
+    expect(wrapper.find(ERROR_SELECTOR).text()).toBeTruthy();
+    expect(wrapper.classes()).toContain(FIELD_CLASSES.INVALID);
+
+    wrapper.setProps({ required: false });
+    expect(wrapper.vm.validate()).toBe(true);
+  });
+
+  test('check if the input type is "password"', () => {
+    expect(wrapper
+      .find('input')
+      .attributes()
+      .type).toBe('password');
+  });
+
+  test('shows the visibility toggle', () => {
+    expect(wrapper.find(VISIBILITY_TOGGLE_SELECTOR).exists()).toBe(true);
+  });
+
+  test('toggles the password\'s visibility', () => {
+    const toggle = wrapper.find(VISIBILITY_TOGGLE_SELECTOR);
+
+    toggle.trigger('click');
+    expect(wrapper.find('input').attributes().type).toBe('text');
+    expect(toggle.classes()).toContain(PASSWORD_VISIBLE_CLASS);
+
+    toggle.trigger('click');
+    expect(wrapper.find('input').attributes().type).toBe('password');
+    expect(toggle.classes()).not.toContain(PASSWORD_VISIBLE_CLASS);
+  });
+
+  test('doesn\'t show visibility toggle if empty', () => {
+    wrapper.setProps({ value: '' });
+    expect(wrapper.find(VISIBILITY_TOGGLE_SELECTOR).exists()).toBe(false);
+  });
+
+  test('Supports disabling', () => {
+    wrapper.setProps({
+      disabled: true,
+    });
+    expect(wrapper.find('input').attributes().disabled).toBe('disabled');
+  });
+
+  test('Can render an icon', () => {
+    const ICON_PATH = 'fake/icon.svg';
+
+    expect(wrapper.classes()).not.toContain(FIELD_CLASSES.HAS_ICON);
+    expect(wrapper.find(ICON_SELECTOR).exists()).toBe(false);
+
+    wrapper.setProps({
+      icon: ICON_PATH,
+    });
+
+    expect(wrapper.classes()).toContain(FIELD_CLASSES.HAS_ICON);
+    expect(wrapper.find(ICON_SELECTOR).exists()).toBe(true);
+  });
+});
