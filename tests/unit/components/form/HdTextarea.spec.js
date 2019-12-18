feb5cccd98fde323dcf556a1f2b575fa9451457a
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2019-12-18 17:34:50 +0100

    :white_check_mark: HdTextarea

--- tests/unit/components/form/HdTextarea.spec.js
+++ tests/unit/components/form/HdTextarea.spec.js
@@ -37,46 +37,56 @@ describe('HdTextarea', () => {
     expect(wrapper.find('textarea').element.value).toBe(TEST_VALUE);
   });
 
-  test('updates the field on change of the prop `value`', () => {
-    wrapper.setProps({ value: TEST_VALUE });
-    expect(wrapper.find('textarea').element.value).toBe(TEST_VALUE);
-  });
-
   test('emits `input` event with the right payload', () => {
     wrapper.find('textarea').setValue(TEST_VALUE);
     expect(wrapper.emitted('input')[0][0]).toEqual(TEST_VALUE);
   });
 
-  test('allows setting a helper', () => {
+  test('allows setting a helper', async () => {
     const TEST_HTML = '<b>test</b> test';
+
     wrapper.vm.showHelper(TEST_HTML);
+
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.find(HELPER_SELECTOR).element.innerHTML).toEqual(TEST_HTML);
   });
 
-  test('validates requiredness', () => {
+  test('validates requiredness', async () => {
     wrapper = wrapperFactory({
       propsData: {
         value: '',
         required: true,
       },
     });
+
     expect(wrapper.vm.validate()).toBe(false);
+
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.find(ERROR_SELECTOR).text()).toBeTruthy();
     expect(wrapper.classes()).toContain(FIELD_CLASSES.INVALID);
+
     wrapper.setProps({
       required: false,
     });
+
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.vm.validate()).toBe(true);
   });
 
-  test('Supports disabling', () => {
+  test('Supports disabling', async () => {
     wrapper.setProps({
       disabled: true,
     });
+
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.find('textarea').attributes().disabled).toBe('disabled');
   });
 
-  test('Can render an icon', () => {
+  test('Can render an icon', async () => {
     const ICON_PATH = 'fake/icon.svg';
 
     expect(wrapper.classes()).not.toContain(FIELD_CLASSES.HAS_ICON);
@@ -85,6 +95,8 @@ describe('HdTextarea', () => {
     wrapper.setProps({
       icon: ICON_PATH,
     });
+
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.classes()).toContain(FIELD_CLASSES.HAS_ICON);
     expect(wrapper.find(ICON_SELECTOR).exists()).toBe(true);
