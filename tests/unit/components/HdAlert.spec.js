2dc82cc1208b638d356005b6dfa5cf614e0ad5bd
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2019-12-18 17:29:53 +0100

    :white_check_mark: HdAlert

--- tests/unit/components/HdAlert.spec.js
+++ tests/unit/components/HdAlert.spec.js
@@ -25,7 +25,7 @@ describe('HdAlert', () => {
     expect(wrapper.find(MESSAGE_SELECTOR).element.innerHTML).toBe(CONTENT);
   });
 
-  test('renders the right type', () => {
+  test('renders the right type', async () => {
     // It doesn't make sense to go through all the possible types (we may have many)
     // they share the same logic, so if one works the rest should work as well
     const TEST_TYPE = 'warning';
@@ -34,22 +34,28 @@ describe('HdAlert', () => {
       type: TEST_TYPE,
     });
 
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.classes()).toContain(`alert--${TEST_TYPE}`);
   });
 
-  test('renders a custom icon', () => {
+  test('renders a custom icon', async () => {
     const ICON_URL = 'https://placeholder.pics/svg/24x24';
     wrapper.setProps({
       icon: ICON_URL,
     });
 
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.find(ICON_SELECTOR).attributes().src).toBe(ICON_URL);
   });
 
-  test('doesn\'t render an icon if the prop `no-icon` is true', () => {
+  test('doesn\'t render an icon if the prop `no-icon` is true', async () => {
     wrapper.setProps({
       noIcon: true,
     });
+
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.find(ICON_SELECTOR).exists()).toBe(false);
   });
