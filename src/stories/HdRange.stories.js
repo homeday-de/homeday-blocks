abf8f8b4bf2ca665a145537be71afc38fe16b16a
ilyasmez <ilyas@amezouar.com>
2020-01-31 11:07:27 +0100

    :sparkles: HdRange enhancements (#298)
    * :sparkles: Range Input Component
    
    * :sparkles: :bug: :lipstick: Code review resulsts + disabled status style
    
    * :sparkles: fixes + tests
    
    * :white_check_mark: further tests
    
    * :white_check_mark: further tests
    
    * :sparkles: PR reviews
    
    * :sparkles: moved the range input scss vars to a proper file
    
    * :white_check_mark: further tests
    
    * :sparkles: step bullets
    
    * :lipstick: UI fix for step bullets
    
    * wip
    
    * WIP: update snapshot (fix Invalid array length)
    
    * :lipstick: UI fix for step bullets
    
    * WIP
    
    * 📸 Update HdRange snapshot
    
    * :bug: Recalculate the positioning on resize
    
    * :bug: Add the scss mixin import
    
    * :camera_flash: Update HdRange snapshot
    
    * :art: Adjust the props naming and the value binding
    
    * :pencil: Write more HdRange stories
    
    * :bug: Fix the value adjustments
    
    * ✅ Enhance and add more test to HdRange
    
    * :zap: Save the width of the track to avoid multiple reflows with offsetWidth
    
    * :bug: Fix the step click handler
    
    * ✅ Cover the step bullets click and the resize logic with tests
    
    * :ok_hand: Remove undefined props and set empty string as background default
    
    Co-authored-by: Daniele Rapisarda <info@danielerapisarda.com>

--- src/stories/HdRange.stories.js
+++ src/stories/HdRange.stories.js
@@ -3,7 +3,11 @@ import { storiesOf } from '@storybook/vue';
 
 import HdRange from 'hd-blocks/components/form/HdRange.vue';
 import FormWrapper from 'hd-blocks/storiesWrappers/FormWrapper';
-import { number, boolean } from '@storybook/addon-knobs';
+import {
+  number,
+  boolean,
+  text,
+} from '@storybook/addon-knobs';
 
 storiesOf('Form/HdRange', module)
   .addDecorator(FormWrapper)
@@ -12,40 +16,40 @@ storiesOf('Form/HdRange', module)
     template: `
       <div>
         <HdRange
-          :minValue="minValue"
-          :maxValue="maxValue"
-          :rangeStep="rangeStep"
+          :min="min"
+          :max="max"
+          :step="step"
           :disabled="disabled"
           :displayStepBullets="displayStepBullets"
           v-model="currentValue"
         />
-        <p>This values {{ currentValue }}</p>
+        <p>Value: {{ currentValue }}</p>
       </div>
     `,
     props: {
       value: {
         type: Number,
-        default: number('Value', 50),
+        default: number('value', 50),
       },
-      minValue: {
+      min: {
         type: Number,
-        default: number('Minimum Value', 0),
+        default: number('min', 0),
       },
-      maxValue: {
+      max: {
         type: Number,
-        default: number('Maximum Value', 100),
+        default: number('max', 100),
       },
-      rangeStep: {
+      step: {
         type: Number,
-        default: number('Step', 10),
+        default: number('step', 10),
       },
       disabled: {
         type: Boolean,
-        default: boolean('Disabled', false),
+        default: boolean('disabled', false),
       },
       displayStepBullets: {
         type: Boolean,
-        default: boolean('Display step bullets', true),
+        default: boolean('displayStepBullets', false),
       },
     },
     watch: {
@@ -59,19 +63,19 @@ storiesOf('Form/HdRange', module)
       };
     },
   }))
-  .add('without bullets', () => ({
+  .add('with bullets', () => ({
     components: { HdRange },
     template: `
       <div>
         <HdRange
-          :minValue="0"
-          :maxValue="100"
-          :rangeStep="20"
+          :min="0"
+          :max="100"
+          :step="20"
           :disabled="false"
-          :displayStepBullets="false"
+          displayStepBullets
           v-model="currentValue"
         />
-        <p>This values {{ currentValue }}</p>
+        <p>Value: {{ currentValue }}</p>
       </div>
     `,
     data() {
@@ -80,22 +84,115 @@ storiesOf('Form/HdRange', module)
       };
     },
   }))
-  .add('without steps', () => ({
+  .add('with tooltip', () => ({
     components: { HdRange },
     template: `
       <div>
         <HdRange
-          :minValue="0"
-          :maxValue="100"
-          :disabled="false"
+          :min="0"
+          :max="100"
+          :step="10"
+          displayTooltip
           v-model="currentValue"
         />
-        <p>This values {{ currentValue }}</p>
+        <p>Value: {{ currentValue }}</p>
       </div>
     `,
     data() {
       return {
         currentValue: 50,
+      };
+    },
+  }))
+  .add('with custom tooltip value', () => ({
+    components: { HdRange },
+    template: `
+      <div>
+        <HdRange
+          v-model="currentValue"
+          :min="0"
+          :max="6"
+          displayTooltip
+          :tooltipValue="tooltipValue"
+        />
+        <p>Value: {{ currentValue }}</p>
+      </div>
+    `,
+    data() {
+      return {
+        currentValue: 0,
+      };
+    },
+    computed: {
+      tooltipValue() {
+        return [
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday',
+        ][this.currentValue];
+      },
+    },
+  }))
+  .add('with labels', () => ({
+    components: { HdRange },
+    template: `
+      <div>
+        <HdRange
+          v-model="currentValue"
+          :min="0"
+          :max="6"
+          displayStepBullets
+          :labels="labels"
+        />
+        <p>Value: {{ currentValue }}</p>
+      </div>
+    `,
+    data() {
+      return {
+        currentValue: 0,
+        labels: [
+          'Mon.',
+          'Tue.',
+          'Wed.',
+          'Thu.',
+          'Fri.',
+          'Sat.',
+          'Sun.',
+        ],
+      };
+    },
+  }))
+  .add('custom backgrounds 🎛', () => ({
+    components: { HdRange },
+    template: `
+      <div>
+        <HdRange
+          v-model="currentValue"
+          :min="0"
+          :max="4"
+          :track-background="trackBackground"
+          :progress-background="progressBackground"
+        />
+        <p>Value: {{ currentValue }}</p>
+      </div>
+    `,
+    props: {
+      trackBackground: {
+        type: String,
+        default: text('track-background', 'radial-gradient(circle at center, #4CBA38, #FFE713, #E00016)'),
+      },
+      progressBackground: {
+        type: String,
+        default: text('progress-background', 'transparent'),
+      },
+    },
+    data() {
+      return {
+        currentValue: 2,
       };
     },
   }));
