2d8211cea981a073cdf344efc82d12e1cc49ed4d
Daniele Rapisarda <info@danielerapisarda.com>
2020-02-06 15:16:19 +0100

    :bug: review from PR comments

--- src/components/HdResponsive.vue
+++ src/components/HdResponsive.vue
@@ -43,19 +43,19 @@ export default {
         ...this.breakpoints,
       };
 
-      for (const breakpointKey in breakpoints) {
-        this.listeners[breakpointKey] = window.matchMedia(breakpoints[breakpointKey]);
-        this.matches[breakpointKey] = this.listeners[breakpointKey].matches;
+      Object.entries(breakpoints).forEach(([key, value]) => {
+        this.listeners[key] = window.matchMedia(value);
+        this.matches[key] = this.listeners[key].matches;
         try {
-          this.listeners[breakpointKey].addEventListener('change', (e) => {
-            this.updateMatch(breakpointKey, e.matches);
+          this.listeners[key].addEventListener('change', (e) => {
+            this.updateMatch(key, e.matches);
           });
         } catch (error) {
-          this.listeners[breakpointKey].addListener((e) => {
-            this.updateMatch(breakpointKey, e.matches);
+          this.listeners[key].addListener((e) => {
+            this.updateMatch(key, e.matches);
           });
         }
-      }
+      });
     },
   },
   render() {
