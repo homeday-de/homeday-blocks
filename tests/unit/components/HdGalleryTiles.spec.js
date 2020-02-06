ff7195ce7ee53e17966a8c1345215218e9e7d6ce
Daniele Rapisarda <info@danielerapisarda.com>
2020-02-06 16:11:38 +0100

    :white_check_mark: tests for HDGalleryTiles

--- tests/unit/components/HdGalleryTiles.spec.js
+++ tests/unit/components/HdGalleryTiles.spec.js
@@ -8,12 +8,27 @@ describe('HdGalleryTiles', () => {
   beforeEach(() => {
     wrapper = mount(HdGalleryTiles, {
       propsData: {
-        item: ITEMS,
+        items: ITEMS,
       },
     });
   });
 
   it('renders component', () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('emits the proper event when the user clicks on an image', () => {
+    const chosenImageIndex = 2;
+    const image = wrapper.findAll('.gallery-tile').at(chosenImageIndex);
+    image.trigger('click');
+
+    expect(wrapper.emitted('itemClick')[0][0]).toBe(chosenImageIndex);
+  });
+
+  it('emits the proper event clicking on the _more photos_ button', () => {
+    const button = wrapper.findAll('.gallery-tile__more-photos');
+    button.trigger('click');
+
+    expect(wrapper.emitted('itemClick')[0][0]).toBe(0);
   });
 });
