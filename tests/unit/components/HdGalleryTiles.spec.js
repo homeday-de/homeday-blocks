39d76dc82150dcac278bf3c23d4137fd05fd1263
Daniele Rapisarda <info@danielerapisarda.com>
2020-02-13 15:33:19 +0100

    [T] Tests for HDGalleryTiles (#314)
    * :white_check_mark: tests for HDGalleryTiles
    
    * :sparkles: test keydown on images of HDGalleryTiles
    
    * :sparkles: selector as parameters

--- tests/unit/components/HdGalleryTiles.spec.js
+++ tests/unit/components/HdGalleryTiles.spec.js
@@ -2,18 +2,46 @@ import { mount } from '@vue/test-utils';
 import HdGalleryTiles from '@/components/gallery/HdGalleryTiles.vue';
 import ITEMS from '@/stories/mocks/GALLERY_ITEMS';
 
+const TILE_SELECTOR = '.gallery-tile';
+const SECOND_TILE_SELECTOR = '.gallery-tiles__container div:nth-child(2)';
+const MORE_PHOTOS_SELECTOR = '.gallery-tile__more-photos';
+
 describe('HdGalleryTiles', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = mount(HdGalleryTiles, {
       propsData: {
-        item: ITEMS,
+        items: ITEMS,
       },
     });
   });
 
   it('renders component', () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('emits the proper event when the user clicks on an image', () => {
+    const chosenImageIndex = 2;
+    const image = wrapper.findAll(TILE_SELECTOR).at(chosenImageIndex);
+    image.trigger('click');
+
+    expect(wrapper.emitted('itemClick')[0][0]).toBe(chosenImageIndex);
+  });
+
+  it('emits the proper event clicking on the _more photos_ button', () => {
+    const button = wrapper.findAll(MORE_PHOTOS_SELECTOR);
+    button.trigger('click');
+
+    expect(wrapper.emitted('itemClick')[0][0]).toBe(0);
+  });
+
+  it('emits the proper event with keydown on an image', () => {
+    const image = wrapper.find(SECOND_TILE_SELECTOR);
+    image.trigger('keydown.space');
+    expect(wrapper.emitted('itemClick').length).toBe(1);
+
+    image.trigger('keydown.enter');
+    expect(wrapper.emitted('itemClick').length).toBe(2);
   });
 });
