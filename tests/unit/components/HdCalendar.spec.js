01985efc04f9c8a35ac2fc121830354dfa32b9e8
Daniele Rapisarda <info@danielerapisarda.com>
2020-02-26 16:41:57 +0100

    :white_check_mark: HdCalendar further testes

--- tests/unit/components/HdCalendar.spec.js
+++ tests/unit/components/HdCalendar.spec.js
@@ -16,4 +16,25 @@ describe('HdCalendar', () => {
   test('renders component', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  test('as a user I\'m able to change week', () => {
+    const setWeekIndexTailMock = jest.fn();
+    wrapper.setMethods({ setWeekIndexTail: setWeekIndexTailMock });
+
+    wrapper.find('.calendar__button--next').trigger('click');
+    expect(setWeekIndexTailMock).toHaveBeenCalledTimes(1);
+
+    wrapper.find('.calendar__button--prev').trigger('click');
+    expect(setWeekIndexTailMock).toHaveBeenCalledTimes(2);
+  });
+
+  test('as a user I\'m able to select a day', () => {
+    const selectDateMock = jest.fn();
+    wrapper.setMethods({ selectDate: selectDateMock });
+
+    const firstDate = wrapper.find('.calendar__date');
+    firstDate.trigger('click');
+    expect(selectDateMock).toHaveBeenCalled();
+    expect(firstDate.classes()).toContain('calendar__date--selected');
+  });
 });
