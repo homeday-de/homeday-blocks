e09545592c488db205902625a0a3dd06ce34fc92
ilyasmez <ilyas@amezouar.com>
2020-02-27 19:49:58 +0100

    :sparkle: Default breakpoints for the responsive service

--- src/services/breakpoints.js
+++ src/services/breakpoints.js
@@ -1,26 +1,36 @@
-let breakpoints = {};
+import sassVariables from 'homeday-blocks/src/styles/variablesForJS.scss';
 
-export const matchMediaAvailable = (
-  typeof window !== 'undefined'
-  && typeof window.matchMedia === 'function'
-);
+let breakpoints = {
+  mobile: `(max-width: ${sassVariables.breakMobile - 1}px)`,
+  tablet: `(min-width: ${sassVariables.breakMobile}px)`,
+  desktop: `(min-width: ${sassVariables.breakTablet}px)`,
+  desktopWide: `(min-width: ${sassVariables.breakDesktop}px)`,
+  desktopExtraWide: `(min-width: ${sassVariables.breakDesktopWide}px)`,
+};
 
-export const mediaMatches = (breakpoint, {
-  matchMedia = matchMediaAvailable ? window.matchMedia : () => false,
-} = {}) => {
+export const matchMediaAvailable = window && typeof window.matchMedia === 'function';
+
+const defaultMatchMedia = matchMediaAvailable ? window.matchMedia : () => false;
+
+export function mediaMatches(
+  breakpoint,
+  { matchMedia = defaultMatchMedia } = {},
+) {
   if (typeof breakpoints[breakpoint] === 'undefined') {
     // Breakpoint not defined, we treat it as a media query string
     return matchMedia(breakpoint).matches;
   }
 
   return matchMedia(breakpoints[breakpoint]).matches;
-};
+}
 
-export const setBreakpoints = (newBreakpoints) => {
+export function setBreakpoints(newBreakpoints) {
   breakpoints = newBreakpoints;
-};
+}
 
-export const getBreakpoints = () => breakpoints;
+export function getBreakpoints() {
+  return breakpoints;
+}
 
 export default {
   matchMediaAvailable,
