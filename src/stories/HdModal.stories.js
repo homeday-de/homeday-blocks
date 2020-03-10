baaf321626320a8ac2772b8507f84e0e09a00083
Aymen Bareche <aym3nb@gmail.com>
2020-03-10 15:12:18 +0100

    :sparkles: Add HdModal

---
+++ src/stories/HdModal.stories.js
@@ -0,0 +1,117 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+import { HdModal } from 'homeday-blocks';
+
+const stories = storiesOf('HdModal', module);
+
+stories.add('Default', () => ({
+  components: { HdModal },
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.modalOpen = true;
+    },
+    hideModal() {
+      this.modalOpen = false;
+    },
+  },
+  template: `
+    <div>
+      <button @click="showModal">show modal</button>
+      <HdModal
+        v-if="modalOpen"
+        @close="hideModal"
+      >
+        <div slot="header">This is the modal header.</div>
+        <div slot="content">This is the modal content.</div>
+       </HdModal>
+    </div>
+  `,
+}));
+
+stories.add('Light', () => ({
+  components: { HdModal },
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.modalOpen = true;
+    },
+    hideModal() {
+      this.modalOpen = false;
+    },
+  },
+  template: `
+    <div>
+      <button @click="showModal">show modal</button>
+      <HdModal
+        v-if="modalOpen"
+        @close="hideModal"
+        closeIconColor="light"
+        overlayColor="light"
+      >
+        <div 
+        slot="header"
+        style="background:red;"
+        >This is the modal header.
+        </div>
+        <div 
+        slot="content"
+        >This is the modal content.
+        </div>
+       </HdModal>
+    </div>
+  `,
+}));
+
+stories.add('With Actions', () => ({
+  components: { HdModal },
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.modalOpen = true;
+    },
+    hideModal() {
+      this.modalOpen = false;
+    },
+    save() {
+      this.hideModal();
+      // eslint-disable-next-line no-alert
+      alert('saved!');
+    },
+  },
+  template: `
+    <div>
+      <button @click="showModal">show modal</button>
+      <HdModal
+        v-if="modalOpen"
+        @close="hideModal"
+        :actions="[
+          {
+            label: 'Cancel',
+            callback: hideModal,
+          },
+          {
+            label: 'Save',
+            callback: save,
+            type: 'primary',
+          },
+        ]"
+      >
+        <div slot="header">This is the modal header.</div>
+        <div slot="content">This is the modal content.</div>
+       </HdModal>
+    </div>
+  `,
+}));
