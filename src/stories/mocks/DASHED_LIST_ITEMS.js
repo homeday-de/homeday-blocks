d10878814655b7625f3831edb5beab71b6ee6251
Aymen Bareche <aym3nb@gmail.com>
2020-03-19 10:32:39 +0100

    :sparkles: Add HdDashedList

---
+++ src/stories/mocks/DASHED_LIST_ITEMS.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'item',
+    value: '1',
+  },
+  {
+    label: 'item',
+    value: '2',
+  },
+  {
+    label: 'item',
+    value: '3',
+  },
+  {
+    label: 'item',
+    value: '4',
+  },
+  {
+    label: 'item',
+    value: '5',
+  },
+];
