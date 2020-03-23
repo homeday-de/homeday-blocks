94dcc85f10caceec92d51916291f43adddc3ce40
Aymen Bareche <aym3nb@gmail.com>
2020-03-23 17:24:59 +0100

    :ok_hand: test strength meter

--- tests/unit/components/form/HdPasswordConfirm.spec.js
+++ tests/unit/components/form/HdPasswordConfirm.spec.js
@@ -1,13 +1,19 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import HdPasswordConfirm from '@/components/form/HdPasswordConfirm.vue';
 import HdInputPassword from '@/components/form/HdInputPassword.vue';
 
 const STRENGTH_METER_SELECTOR = '.confirmPassword__strengthMeter';
+const STRENGTH_BAR_VISIBLE_CLASS = 'isVisible';
+
+const TEST_PASSWORD = 'loremIpsum123';
 
 describe('HdPasswordConfirm', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallowMount(HdPasswordConfirm);
+    wrapper = mount(HdPasswordConfirm, {
+      propsData: { value: TEST_PASSWORD },
+      sync: false, // Added to avoid this issue : https://github.com/vuejs/vue-test-utils/issues/532
+    });
   });
 
   it('is rendered as expected', () => {
@@ -18,7 +24,27 @@ describe('HdPasswordConfirm', () => {
     expect(wrapper.findAll(HdInputPassword).length).toEqual(2);
   });
 
-  it('has a password strength meter', () => {
-    expect(wrapper.find(STRENGTH_METER_SELECTOR).exists()).toBeTruthy();
+  it('shows password strength as text by default', async () => {
+    await wrapper.vm.validate();
+
+    expect(wrapper.find(STRENGTH_METER_SELECTOR).text()).toBeTruthy();
+  });
+
+  it('does not show the password strength if the prop `withStrength` is set to false', async () => {
+    wrapper.setProps({ withStrength: false });
+
+    await wrapper.vm.validate();
+
+    expect(wrapper.find(STRENGTH_METER_SELECTOR).text()).toBeFalsy();
+  });
+
+  it('shows password strength as a bar if the prop `strengthBarStyle` is set to true', async () => {
+    expect(wrapper.find(STRENGTH_METER_SELECTOR).classes()).not.toContain(STRENGTH_BAR_VISIBLE_CLASS);
+
+    wrapper.setProps({ strengthBarStyle: true });
+
+    await wrapper.vm.validate();
+
+    expect(wrapper.find(STRENGTH_METER_SELECTOR).classes()).toContain(STRENGTH_BAR_VISIBLE_CLASS);
   });
 });
