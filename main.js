917068c810a2ae44350cdd91b8e308d46a781dd8
Ilyas <ilyas@amezouar.com>
2020-03-26 14:37:41 +0100

    🔁 Sync branch to develop and adapt HdModal

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ export { default as HdTagsList } from './src/components/HdTagsList.vue';
 export { default as HdTimeslots } from './src/components/HdTimeslots.vue';
 export { default as HdToast } from './src/components/HdToast.vue';
 export { default as HdIcon } from './src/components/HdIcon.vue';
+export { default as HdModal } from './src/components/HdModal.vue';
 
 // Buttons
 export { default as HdArrowButton } from './src/components/buttons/HdArrowButton.vue';
