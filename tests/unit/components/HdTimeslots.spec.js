e28957dac0b1dd3a5be22966d41e347160548de0
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-04-06 18:05:29 +0200

    :recycle: HdTimeslots

--- tests/unit/components/HdTimeslots.spec.js
+++ tests/unit/components/HdTimeslots.spec.js
@@ -1,55 +1,64 @@
-import { mount } from '@vue/test-utils';
+import { wrapperFactoryBuilder } from 'tests/unit/helpers';
 import HdTimeslots from '@/components/HdTimeslots.vue';
 import generateSlots from '@/stories/HdTimeslots.stories';
 
+const defaultTimeslots = generateSlots(400, 20);
+const defaultTimeslotsPerPage = Math.floor(defaultTimeslots.length / 2);
+
+const wrapperBuilder = wrapperFactoryBuilder(HdTimeslots, {
+  props: {
+    timeslots: defaultTimeslots,
+    timeslotsPerPage: defaultTimeslotsPerPage,
+  },
+});
+
 describe('HdTimeslots', () => {
-  let wrapper;
-  const testTimeslots = generateSlots(400, 20);
-  // the page is shorter then the timeslots quantity
-  const testTimeslotsPerPage = Math.floor(testTimeslots.length / 2);
-  const getDisplayedTimeslots = () => wrapper.findAll('.timeslots__slot');
-  const getDisplayedTimeslotsHtml = () => wrapper.findAll('.timeslots__slot').wrappers.reduce((acc, element) => acc + element.html(), '');
+  const getDisplayedTimeslots = wrapper => wrapper.findAll('.timeslots__slot');
 
-  beforeEach(() => {
-    wrapper = mount(HdTimeslots, {
-      propsData: {
-        timeslots: testTimeslots,
-        timeslotsPerPage: testTimeslotsPerPage,
-      },
-    });
-  });
+  it('once the component is mounted, the first slot is selected', async () => {
+    const wrapper = wrapperBuilder();
+    const firstTimeslot = getDisplayedTimeslots(wrapper).at(0);
 
-  test.skip('renders component', () => {
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  test('once the component is mounted, the first slot is selected', () => {
-    const firstTimeslot = getDisplayedTimeslots().at(0);
+    await wrapper.vm.$nextTick();
 
     expect(firstTimeslot.classes()).toContain('timeslots__slot--isSelected');
   });
 
-  test('clicking on a timeslot, the timeslot selecting method is called', () => {
+  it('clicking on a timeslot, the timeslot selecting method is called', () => {
     const mockedSelectTimeslot = jest.fn();
-    const displayedTimeslots = getDisplayedTimeslots();
+    const wrapper = wrapperBuilder({
+      methods: {
+        selectTimeslot: mockedSelectTimeslot,
+      },
+    });
+    const displayedTimeslots = getDisplayedTimeslots(wrapper);
     const randomTimeslot = displayedTimeslots.at(Math.floor(Math.random() * displayedTimeslots.length));
-    wrapper.setMethods({ selectTimeslot: mockedSelectTimeslot });
+
     randomTimeslot.trigger('click');
 
     expect(mockedSelectTimeslot).toHaveBeenCalled();
   });
 
-  test('clicking on a timeslot, the timeslot is selected', () => {
-    const displayedTimeslots = getDisplayedTimeslots();
+  it('clicking on a timeslot, the timeslot is selected', async () => {
+    const wrapper = wrapperBuilder();
+    const displayedTimeslots = getDisplayedTimeslots(wrapper);
     const randomTimeslot = displayedTimeslots.at(Math.floor(Math.random() * displayedTimeslots.length));
+
     randomTimeslot.trigger('click');
+
+    await wrapper.vm.$nextTick();
 
     expect(randomTimeslot.classes()).toContain('timeslots__slot--isSelected');
   });
 
-  test('clicking on arrows, the page change methods are called', () => {
+  it('clicking on arrows, the page change methods are called', () => {
     const mockedFlipPage = jest.fn();
-    wrapper.setMethods({ flipPage: mockedFlipPage });
+    const wrapper = wrapperBuilder({
+      methods: {
+        flipPage: mockedFlipPage,
+      },
+    });
+
     wrapper.find('.arrowButton--right').trigger('click');
 
     expect(mockedFlipPage).toHaveBeenCalledTimes(1);
@@ -59,28 +68,20 @@ describe('HdTimeslots', () => {
     expect(mockedFlipPage).toHaveBeenCalledTimes(2);
   });
 
-  test('clicking on arrows, the page is changed', () => {
-    const initialTimeslotsListContent = getDisplayedTimeslotsHtml();
-    wrapper.find('.arrowButton--right').trigger('click');
-
-    expect(getDisplayedTimeslotsHtml()).not.toBe(initialTimeslotsListContent);
-
-    wrapper.find('.arrowButton--left').trigger('click');
-
-    expect(getDisplayedTimeslotsHtml()).toBe(initialTimeslotsListContent);
-  });
-
-  test('slots are displayed in the proper quantity', () => {
+  it('slots are displayed in the proper quantity', async () => {
+    const wrapper = wrapperBuilder();
     const { timeslots } = wrapper.vm.$options.props;
 
     expect(timeslots.validator(Array.from(timeslots))).toBe(true);
-    expect(getDisplayedTimeslots().length).toBe(testTimeslotsPerPage);
+    expect(getDisplayedTimeslots(wrapper).length).toBe(defaultTimeslotsPerPage);
 
     // the page is now wider then the timeslots quantity
-    wrapper.setProps({ timeslotsPerPage: testTimeslots.length * 2 });
+    wrapper.setProps({ timeslotsPerPage: defaultTimeslots.length * 2 });
 
-    expect(getDisplayedTimeslots().length).toBe(testTimeslots.length);
+    await wrapper.vm.$nextTick();
+
+    expect(getDisplayedTimeslots(wrapper).length).toBe(defaultTimeslots.length);
     // data validity check
-    expect(testTimeslots.length).toBeGreaterThan(0);
+    expect(defaultTimeslots.length).toBeGreaterThan(0);
   });
 });
