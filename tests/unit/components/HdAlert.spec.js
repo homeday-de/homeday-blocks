375a8c2f296b07bc7ae29c9ead12cffaad20fea7
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-04-06 15:22:57 +0200

    :recycle: HdAlert

--- tests/unit/components/HdAlert.spec.js
+++ tests/unit/components/HdAlert.spec.js
@@ -1,54 +1,60 @@
-import { shallowMount } from '@vue/test-utils';
+import { wrapperFactoryBuilder } from 'tests/unit/helpers';
 import HdAlert from '@/components/HdAlert.vue';
+
+const defaultSlot = '<b>Default slot</b>';
+
+const wrapperBuilder = wrapperFactoryBuilder(HdAlert, {
+  slots: {
+    default: defaultSlot,
+  },
+  shallow: true,
+});
 
 const MESSAGE_SELECTOR = '.alert__message';
 const ICON_SELECTOR = '.alert__icon';
 
-const CONTENT = '<b>Default slot</b>';
-
 describe('HdAlert', () => {
-  let wrapper;
+  it('renders component', () => {
+    const wrapper = wrapperBuilder();
 
-  beforeEach(() => {
-    wrapper = shallowMount(HdAlert, {
-      slots: {
-        default: CONTENT,
-      },
-    });
-  });
-
-  test('renders component', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  test('renders the slot correctly', () => {
-    expect(wrapper.find(MESSAGE_SELECTOR).element.innerHTML).toBe(CONTENT);
+  it('renders the slot correctly', () => {
+    const wrapper = wrapperBuilder();
+
+    expect(wrapper.find(MESSAGE_SELECTOR).element.innerHTML).toBe(defaultSlot);
   });
 
-  test('renders the right type', () => {
+  it('renders the right type', () => {
+    const TEST_TYPE = 'warning';
+    const wrapper = wrapperBuilder({
+      props: {
+        type: TEST_TYPE,
+      },
+    });
     // It doesn't make sense to go through all the possible types (we may have many)
     // they share the same logic, so if one works the rest should work as well
-    const TEST_TYPE = 'warning';
-
-    wrapper.setProps({
-      type: TEST_TYPE,
-    });
 
     expect(wrapper.classes()).toContain(`alert--${TEST_TYPE}`);
   });
 
-  test('renders a custom icon', () => {
+  it('renders a custom icon', () => {
     const ICON_URL = 'https://placeholder.pics/svg/24x24';
-    wrapper.setProps({
-      icon: ICON_URL,
+    const wrapper = wrapperBuilder({
+      props: {
+        icon: ICON_URL,
+      },
     });
 
     expect(wrapper.find(ICON_SELECTOR).attributes().src).toBe(ICON_URL);
   });
 
-  test('doesn\'t render an icon if the prop `no-icon` is true', () => {
-    wrapper.setProps({
-      noIcon: true,
+  it('doesn\'t render an icon if the prop `no-icon` is true', () => {
+    const wrapper = wrapperBuilder({
+      props: {
+        noIcon: true,
+      },
     });
 
     expect(wrapper.find(ICON_SELECTOR).exists()).toBe(false);
