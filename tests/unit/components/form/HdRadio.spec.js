58957070febf5c8aa612544538a565fd0b539f6f
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-04-07 16:31:25 +0200

    :recycle: HdRadio

--- tests/unit/components/form/HdRadio.spec.js
+++ tests/unit/components/form/HdRadio.spec.js
@@ -11,42 +11,47 @@ const SELECTED_ITEM_SELECTOR = '.isSelected';
 const VERTICAL_CLASSNAME = 'radio-wrapper--vertical';
 const ITEMS_SELECTOR = { ref: 'items' };
 
+const wrapperBuilder = wrapperFactoryBuilder(HdRadio, {
+  props: {
+    name: 'testRadio',
+    label: 'test radio label',
+    items: ITEMS,
+  },
+});
+
 describe('HdRadio', () => {
-  const wrapperFactory = wrapperFactoryBuilder(HdRadio, {
-    propsData: {
-      name: 'testRadio',
-      label: 'test radio label',
-      items: ITEMS,
-    },
-  });
+  it('is rendered as expected', () => {
+    const wrapper = wrapperBuilder();
 
-  let wrapper;
-  beforeEach(() => {
-    wrapper = wrapperFactory();
-  });
-
-  test('is rendered as expected', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  test('renders with preselected item', () => {
-    wrapper = wrapperFactory({
-      propsData: {
+  it('renders with preselected item', () => {
+    const wrapper = wrapperBuilder({
+      props: {
         value: ITEMS[2].value,
       },
     });
+
     expect(wrapper.findAll(SELECTED_ITEM_SELECTOR).length).toBe(1);
   });
 
-  test('emits `input` event with the right payload on click', () => {
+  it('emits `input` event with the right payload on click', () => {
+    const wrapper = wrapperBuilder();
     const ITEM_INDEX = 1;
+
     wrapper.findAll(ITEMS_SELECTOR).at(ITEM_INDEX).trigger('click');
+
     expect(wrapper.emitted('input')[0][0]).toEqual(ITEMS[ITEM_INDEX].value);
   });
 
-  test('emits `input` event with the right payload on arrows keydown', () => {
+  it('emits `input` event with the right payload on arrows keydown', () => {
     const INITIAL_ITEM_INDEX = 2;
-    wrapper.setProps({ value: ITEMS[INITIAL_ITEM_INDEX].value });
+    const wrapper = wrapperBuilder({
+      props: {
+        value: ITEMS[INITIAL_ITEM_INDEX].value,
+      },
+    });
     const getLastInputEventPayload = () => getLastEventPayload({ wrapper, eventName: 'input' });
     // The keydown event handler is the same for all the items
     // so it doesn't matter which items fires it. We pick the first one in this test
@@ -90,30 +95,40 @@ describe('HdRadio', () => {
     });
   });
 
-  test('can show items vertically', () => {
-    wrapper.setProps({
-      vertical: true,
+  it('can show items vertically', () => {
+    const wrapper = wrapperBuilder({
+      props: {
+        vertical: true,
+      },
     });
+
     expect(wrapper.classes()).toContain(VERTICAL_CLASSNAME);
   });
 
-  test('validates requiredness', () => {
-    wrapper.setProps({
-      value: undefined, // or any other `value` that we don't have in the array of items
+  it('validates requiredness', () => {
+    const wrapper = wrapperBuilder({
+      props: {
+        value: undefined,
+      },
     });
     // We make sure nothing is selected so we can properly test the validation
     expect(wrapper.findAll(SELECTED_ITEM_SELECTOR).length).toBe(0);
     expect(wrapper.vm.validate()).toBe(true);
+
     wrapper.setProps({
       required: true,
     });
+
     expect(wrapper.vm.validate()).toBe(false);
   });
 
-  test('Supports disabling', () => {
-    wrapper.setProps({
-      disabled: true,
+  it('Supports disabling', () => {
+    const wrapper = wrapperBuilder({
+      props: {
+        disabled: true,
+      },
     });
+
     expect(wrapper.find('input').attributes().disabled).toBe('disabled');
   });
 });
