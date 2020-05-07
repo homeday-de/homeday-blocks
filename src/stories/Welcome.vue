e76bda3e1919862966a7aa1c7033bb7dfcc0980f
Ilyas <ilyas@amezouar.com>
2020-05-07 18:53:30 +0200

    :pencil2: Update the welcome page

--- src/stories/Welcome.vue
+++ src/stories/Welcome.vue
@@ -3,27 +3,35 @@
     <h1 class="welcome__title">Homeday-Blocks</h1>
     <p class="welcome__paragraph">A Vue component library built by Homeday's frontend team.</p>
 
-    <h3 class="welcome__subtitle">Start using the library in 3 quick steps</h3>
+    <h3 class="welcome__subtitle">Start using the library in 2 quick steps</h3>
     <p class="welcome__paragraph">
       1- Install the library:
       <vue-code-highlight class="welcome__code">{{
-        `npm install homeday-de/homeday-blocks --save`
+        "npm install homeday-de/homeday-blocks --save"
       }}</vue-code-highlight>
       2- Consume the components
       <vue-code-highlight class="welcome__code">{{
-        'import { HdGallery } from \'homeday-blocks\';'
+        "import { HdGallery } from 'homeday-blocks';"
       }}</vue-code-highlight>
     </p>
 
+    <h3 class="welcome__subtitle">Older browsers support</h3>
+    <p class="welcome__paragraph">
+      As we are serving "raw" components, and babel-loader doesn't transpile the dependencies by default,
+      you might want to ask it to do so.<br />
+      Our recommended solution is to use transpileDependencies in the Vue config file:
+      <vue-code-highlight class="welcome__code">{{
+        "transpileDependencies: ['homeday-blocks', 'vue-zoomer'],"
+      }}</vue-code-highlight>
+    </p>
     <h3 class="welcome__subtitle">How to use our storybook?</h3>
-    <div class="welcome__paragraph">
-      <h4>You don't have to check all the tabs to see if something is available. We use some suffixes in the story's title:</h4>
-      <div class="text-small margin-top-s margin-left-m">
-        <p>🎛 : Check the knobs tab to play around with the component's props</p>
-        <p>📝 : Check the notes tab for more information about the component, how to use it or code snippets</p>
-      </div>
-    </div>
-
+    <h4>You don't have to check all the tabs to see if something is available. We use some suffixes in the story's title:</h4>
+    <p class="welcome__paragraph">
+      <ul class="text-small margin-top-s margin-left-m">
+        <li>🎛 : Check the knobs tab to play around with the component's props</li>
+        <li>📝 : Check the notes tab for more information about the component, how to use it or code snippets</li>
+      </ul>
+    </p>
     <h3 class="welcome__subtitle">Contributing :)</h3>
     <p class="welcome__paragraph">Check out our <a class="link" href="https://github.com/homeday-de/homeday-blocks">Github repository</a> for more details.</p>
   </div>
