4e677f21efb0484c75f751b02e58276cbf88397d
Daniel González <d.g.g69@me.com>
2020-06-03 23:24:47 -0500

    :sparkles: create HdDatePicker component

---
+++ src/components/form/HdDatePicker.vue
@@ -0,0 +1,49 @@
+<template>
+  <date-pick
+    v-bind="{...$props, ...$attrs}"
+    :value="value"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+import DatePick from 'vue-date-pick';
+import 'vue-date-pick/dist/vueDatePick.css';
+
+export default {
+  components: { DatePick },
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    displayFormat: {
+      type: String,
+      default: 'YYYY.MM.DD',
+    },
+    weekdays: {
+      type: Array,
+      default: () => ([
+        'So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa',
+      ]),
+    },
+    months: {
+      type: Array,
+      default: () => ([
+        'Januar', 'Februar', 'März', 'April',
+        'Mai', 'Juni', 'Juli', 'August',
+        'September', 'Oktober', 'November', 'Dezember',
+      ]),
+    },
+    startWeekOnSunday: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleInput(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
