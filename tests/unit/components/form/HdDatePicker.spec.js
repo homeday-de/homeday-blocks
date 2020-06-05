f19338714638e830874e67352d9c7b3aff2a4a79
Daniel González <d.g.g69@me.com>
2020-06-04 22:59:45 -0500

    :white_check_mark: add prefilled value test to HdDatePicker

--- tests/unit/components/form/HdDatePicker.spec.js
+++ tests/unit/components/form/HdDatePicker.spec.js
@@ -2,6 +2,7 @@ import { wrapperFactoryBuilder } from 'tests/unit/helpers';
 
 import HdDatePicker from '@/components/form/HdDatePicker.vue';
 
+const TEST_VALUE = '2020.06.01';
 const wrapperBuilder = wrapperFactoryBuilder(HdDatePicker, {});
 
 describe('HdDatePicker', () => {
@@ -9,5 +10,15 @@ describe('HdDatePicker', () => {
     const wrapper = wrapperBuilder();
 
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('is rendered with prefilled value', () => {
+    const wrapper = wrapperBuilder({
+      props: {
+        value: TEST_VALUE,
+      },
+    });
+
+    expect(wrapper.find('input').element.value).toBe(TEST_VALUE);
   });
 });
