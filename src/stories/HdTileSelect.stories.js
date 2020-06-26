fdf34e94ea9ec4bb1b5db6dd9cff330ff1ff46eb
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-06-26 11:29:12 +0200

    ✨ HdTileSelect component (#465)
    * :sparkles: HdTileSelect component
    
    Simplified the API a bit and split logic into multiple components.
    
    * :arrow_up: Node v10.21.0
    
    * :bug: Ignore required v-model value
    
    * :ok_hand: Use <div> instead of <section>
    
    From MDN: Do not use the <section> element as a generic container; this is what <div> is for, especially when the sectioning is only for styling purposes. A rule of thumb is that a section should logically appear in the outline of a document.
    
    * :bulb: Use JSDoc for items types
    
    * :pencil2: inpu -> input
    
    * :ok_hand: __input to --input
    
    * :fire: Remove unused classes
    
    * :camera_flash: Remove unused class

---
+++ src/stories/HdTileSelect.stories.js
@@ -0,0 +1,98 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+import { HdTileSelect } from 'homeday-blocks';
+
+const stories = storiesOf('Components|HdTileSelect', module);
+const watch = {
+  value(newValue) {
+    console.log(`Selected value: ${newValue}`);
+  },
+};
+
+stories.add('Simple', () => ({
+  components: { HdTileSelect },
+  template: `
+    <HdTileSelect
+      v-model="value"
+      :items="items"
+    >
+      {{ value }}
+    </HdTileSelect>
+  `,
+  data() {
+    return {
+      items: [1, 2, 3],
+      value: 0,
+    };
+  },
+  watch,
+}));
+
+stories.add('Simple with Object items', () => ({
+  components: { HdTileSelect },
+  template: `
+    <HdTileSelect
+      v-model="value"
+      :items="items"
+    >
+      {{ value }}
+    </HdTileSelect>
+  `,
+  data() {
+    return {
+      items: [
+        {
+          value: true,
+          text: 'Yes',
+        },
+        {
+          value: false,
+          text: 'No',
+        },
+      ],
+      value: '',
+    };
+  },
+  watch,
+}));
+
+stories.add('Custom formatter', () => ({
+  components: { HdTileSelect },
+  template: `
+    <HdTileSelect
+      v-model="value"
+      :items="items"
+      :formatter="formatter"
+    >
+      {{ value }}
+    </HdTileSelect>
+  `,
+  data() {
+    return {
+      items: [1, 2, 3],
+      value: 0,
+      formatter: value => (`Formatted ${value}`),
+    };
+  },
+  watch,
+}));
+
+stories.add('Accept new value', () => ({
+  components: { HdTileSelect },
+  template: `
+    <HdTileSelect
+      v-model="value"
+      :items="items"
+      accept-new-value
+    >
+      {{ value }}
+    </HdTileSelect>
+  `,
+  data() {
+    return {
+      items: [1, 2, 3],
+      value: 0,
+    };
+  },
+  watch,
+}));
