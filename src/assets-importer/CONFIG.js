5b8cd5fd0975a1e8432f5d7f0d11295eb6fa7569
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-07-03 10:46:05 +0200

    :bento: Importing new assets from new master

--- src/assets-importer/CONFIG.js
+++ src/assets-importer/CONFIG.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     {
       name: 'bigIcons',
-      figmaFileKey: 'ZfLfSmSFdNbhNFvwitFIs6',
+      figmaFileKey: 'KdznSjw9btqHEKdgmm2UT4co',
       matchingRegex: new RegExp('Icons XL/ic_', 'i'),
       filenameRegex: new RegExp('ic_.*?(?=_)', 'i'),
       dist: '/src/assets/big-icons',
