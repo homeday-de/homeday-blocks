661fe3edcebc1d96e0c083077bef67b71b447b10
Maja Komel <maja.komel@gmail.com>
2020-07-20 09:54:44 +0200

    ✨ Support title and description in HdIcon (#481)
    * ✨ make HdIcon accessible
    
    * add review changes
    
    * snapshot spec
    
    * reorganize the code, add another test
    
    * fix test
    
    * fix linting 🤦‍♀️
    
    * use addAccessibilityTags

--- src/components/HdIcon.vue
+++ src/components/HdIcon.vue
@@ -1,6 +1,7 @@
 <template>
   <inline-svg
     :src="src"
+    :id="id"
     :transform-source="transform"
     v-bind="$attrs"
   />
@@ -21,6 +22,15 @@ export default {
       type: String,
       required: true,
     },
+    title: {
+      type: String,
+    },
+    id: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
     // to set a fill based on the path's class
     fillFromClass: {
       type: Object,
@@ -34,6 +44,8 @@ export default {
   },
   methods: {
     transform(svg) {
+      this.addAccessibilityTags(svg);
+
       if (!this.fillFromClass && !this.classFromFill) {
         return svg;
       }
@@ -52,6 +64,27 @@ export default {
       });
 
       return svg;
+    },
+    addAccessibilityTags(svg) {
+      const randId = Math.floor(Math.random() * 1000);
+      if (this.title) this.addTitleTag(svg, randId);
+      if (this.description) this.addDescTag(svg, randId);
+    },
+    addTitleTag(svg, randId) {
+      const titleId = `${this.id || randId}-title`;
+      const titleTag = document.createElementNS('http://www.w3.org/2000/svg', 'title');
+      titleTag.textContent = this.title;
+      titleTag.id = titleId;
+      svg.insertBefore(titleTag, svg.firstChild);
+      svg.setAttribute('aria-labelledby', titleId);
+    },
+    addDescTag(svg, randId) {
+      const descId = `${this.id || randId}-desc`;
+      const descTag = document.createElementNS('http://www.w3.org/2000/svg', 'desc');
+      descTag.id = descId;
+      descTag.textContent = this.description;
+      svg.appendChild(descTag);
+      svg.setAttribute('aria-describedby', descId);
     },
     getFillFromClassNames(classNames) {
       if (!classNames || !this.fillFromClass) {
