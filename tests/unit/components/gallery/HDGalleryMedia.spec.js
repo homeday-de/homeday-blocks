97cb843fbc514f6fe05ed291b8858cd053851135
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-08-24 17:24:01 +0200

    ✅ Add tests to the HdGalleryMedia component

--- tests/unit/components/gallery/HDGalleryMedia.spec.js
+++ tests/unit/components/gallery/HDGalleryMedia.spec.js
@@ -1,17 +1,43 @@
+import _ from 'lodash';
 import { wrapperFactoryBuilder } from 'tests/unit/helpers';
 import HdGalleryMedia from '@/components/gallery/HdGalleryMedia.vue';
 import ITEMS from '@/stories/mocks/GALLERY_ITEMS';
 
-const wrapperBuilder = wrapperFactoryBuilder(HdGalleryMedia, {
-  props: {
-    item: ITEMS[0],
-  },
-});
-
 describe('HdGalleryMedia', () => {
-  it('renders component', () => {
-    const wrapper = wrapperBuilder();
+  const build = (props = {}) => {
+    const wrapper = wrapperFactoryBuilder(HdGalleryMedia, {
+      props: _.merge({
+        item: ITEMS[0],
+      }, props),
+    })();
 
+    return {
+      wrapper,
+      img: () => wrapper.find('img'),
+      thumbnail: () => wrapper.find('.gallery-media__object-thumbnail'),
+    };
+  };
+
+  it('renders component', () => {
+    const { wrapper } = build();
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('hides the thumbnail when the image is loaded', async () => {
+    const { wrapper, img, thumbnail } = build();
+
+    expect(thumbnail().classes()).toContain('isVisible');
+
+    img().trigger('load');
+    await wrapper.vm.$nextTick();
+
+    expect(thumbnail().classes()).not.toContain('isVisible');
+  });
+
+  it('hides the thumbnail when is not provided', async () => {
+    const itemWithoutThumb = { ...ITEMS[0], thumbnail: null };
+    const { thumbnail } = build({ item: itemWithoutThumb });
+
+    expect(thumbnail().exists()).toBe(false);
   });
 });
