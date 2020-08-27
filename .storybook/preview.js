364e324ca88133efaf5438a0f84054b3ed7c7ae8
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-08-27 15:12:10 +0200

    ♻️ Sort stories alphabetically (#524)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,6 +9,7 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
+      method: 'alphabetical',
       order: [
         'Introduction'
       ]
