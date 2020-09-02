bfbf730a66d8ac9c9d148b4cc24da3a8d9a5adeb
Daniel González <d.g.g69@me.com>
2020-09-02 07:02:10 -0500

    📌 (GoogleMaps): update to version 3.42 (#525)

--- src/services/googleAPI.js
+++ src/services/googleAPI.js
@@ -8,8 +8,10 @@ export function getGoogleAPI(key) {
   }
 
   mapsApiPromise = new Promise((resolve) => {
+    // Check Google Maps version documentation: https://developers.google.com/maps/documentation/javascript/versions
+    const version = '3.42';
     loadScript({
-      url: `https://maps.googleapis.com/maps/api/js?v=3.37&key=${key}&libraries=places&region=DE`,
+      url: `https://maps.googleapis.com/maps/api/js?v=${version}&key=${key}&libraries=places&region=DE`,
     }).then(() => {
       resolve(window.google);
     }).catch(() => {
