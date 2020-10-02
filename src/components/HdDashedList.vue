b549d44239c29fd16dc32670a7c83ed14e4961ce
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-10-02 10:33:11 +0200

    ♻️ Change DashedList component to the new design system (#568)

--- src/components/HdDashedList.vue
+++ src/components/HdDashedList.vue
@@ -1,13 +1,6 @@
 <template>
-  <dl
-    :class="{
-      'dashed-list--expanded-label': expandedLabel,
-    }"
-    class="dashed-list"
-  >
-    <template
-      v-for="(item, index) in items"
-    >
+  <dl class="dashed-list">
+    <template v-for="(item, index) in items">
       <h4
         v-if="item.title"
         :key="index"
@@ -15,32 +8,24 @@
       >
         {{ item.title }}
       </h4>
+
       <div
         v-for="({ label, value, secondaryValue }, subIndex) in (item.items || [item])"
         :key="`${index}${subIndex}`"
         class="dashed-list__item"
+        :class="{
+          'dashed-list__item--expanded-label': expandedLabel && !secondaryValue,
+        }"
       >
-        <dt class="dashed-list__section dashed-list__section--label">
-          <span
-            class="dashed-list__text"
-            v-html="label"
-          />
+        <dt>
+          <span v-html="label" />
         </dt>
-        <dd class="dashed-list__section">
-          <span
-            class="dashed-list__text"
-            v-html="value"
-          />
+        <dd>
+          <span v-html="value" />
         </dd>
-        <dd
-          v-if="secondaryValue"
-          class="dashed-list__section dashed-list__section--secondary"
-        >
-          <div class="dashed-list__dashes" />
-          <span
-            class="dashed-list__text"
-            v-html="secondaryValue"
-          />
+        <dd v-if="secondaryValue">
+          <div class="dashed-list__dash" />
+          <span v-html="secondaryValue" />
         </dd>
       </div>
     </template>
@@ -66,83 +51,83 @@ export default {
 <style lang="scss" scoped>
 @import 'homeday-blocks/src/styles/mixins.scss';
 
-.dashed-list {
-  $root: &;
-  $leaders-border: 2px dashed getShade($neutral-gray, 50);
-  background-color: inherit;
+$leaders-border: 1px dashed getShade($neutral-gray, 50);
 
-  &__item {
-    display: flex;
-    flex-flow: wrap;
-    margin-bottom: $stack-s;
-  }
+.dashed-list {
+  background-color: inherit;
 
   &__title {
     margin-top: $stack-l;
     margin-bottom: $stack-s;
+    color: $neutral-gray;
+    font-weight: 700;
   }
 
-  &__section {
+  &__dash {
+    flex: 1;
+    display: inline-flex;
+    border-bottom: $leaders-border;
+    margin-bottom: .47em;
+    margin-left: 0.1em;
+    margin-right: 0.1em;
+  }
+}
+
+.dashed-list__item {
+  display: flex;
+  flex-flow: wrap;
+  margin-bottom: $stack-s;
+
+  > dt,
+  > dd {
+    position: relative;
     display: flex;
     align-items: flex-end;
-    position: relative;
     overflow: hidden;
 
-    &--label {
-      width: 100%;
-      color: getShade($quaternary-color, 80);
-
-      @media (min-width: $break-tablet) {
-        width: 40%;
-      }
-
-      #{$root}__text {
-        @media (min-width: $break-tablet) {
-          max-width: 60%;
-
-          &:after {
-            content: "";
-            position: absolute;
-            border-bottom: $leaders-border;
-            margin-left: 2px;
-            margin-top: 1.03em;
-            width: 100%;
-          }
-        }
-      }
-
-      #{$root}__dashes {
-        display: none;
-
-        @media (min-width: $break-tablet) {
-          display: inline;
-        }
-      }
-    }
-    &--secondary {
-      flex: 1;
-      justify-content: flex-end;
-    }
   }
 
-  &--expanded-label {
-    #{$root}__section--label {
-      width: auto;
-
-      #{$root}__text {
-        max-width: unset;
-      }
-    }
+  > dt {
+    width: 100%;
+    color: getShade($neutral-gray, 80);
   }
 
-  &__text {
+  > dd {
     background-color: inherit;
+    color: $cello-blue;
   }
 
-  &__dashes {
+  > dd + dd {
     flex: 1;
-    margin-bottom: .4em;
-    border-bottom: $leaders-border;
+    justify-content: flex-end;
+  }
+}
+
+@media (min-width: $break-tablet) {
+  .dashed-list__item {
+    > dt {
+      position: relative;
+      width: 40%;
+      max-width: 60%;
+      margin-right: 0.1em;
+
+      > span::after {
+        content: "";
+        position: absolute;
+        border-bottom: $leaders-border;
+        width: 100%;
+        margin-left: 0.1em;
+        margin-top: 1em;
+      }
+    }
+  }
+
+  .dashed-list__item--expanded-label {
+    > dt {
+      flex: 1;
+      width: auto;
+      max-width: unset;
+    }
   }
 }
 </style>
