b7c9cb2ff4e2bf6ace9cbe17bb5aa41fc4ee2bf4
Daniele Rapisarda <info@danielerapisarda.com>
2020-10-15 10:10:27 +0200

    :sparkles: Finalized styles

--- src/stories/Typography.stories.js
+++ src/stories/Typography.stories.js
@@ -1,44 +1,68 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import { withKnobs, select, number } from '@storybook/addon-knobs';
+import './styles/Typography.scss';
 
 storiesOf('Typography/Typographies', module)
   .addDecorator(withKnobs)
-  .add('Ready to use Typographies 🎛', () => ({
-    data() {
-      return {
-        typographyClasses: [
-          'ty-minor-second',
-          'ty-minor-third',
-          'ty-major-third',
-          'ty-perfect-fourth',
-          'ty-augmented-fourth',
-          'ty-perfect-fifth',
-          'ty-golden-ratio',
-        ],
-      };
-    },
+  .add('Typography mixins 🎛', () => ({
     props: {
       variation: {
-        default: select('Typography variation', [
-          'ty-minor-second',
-          'ty-minor-third',
-          'ty-major-third',
-          'ty-perfect-fourth',
-          'ty-augmented-fourth',
-          'ty-perfect-fifth',
-          'ty-golden-ratio',
-        ],
-        'ty-minor-second'),
+        default: select('Font variation', [
+          'DS-60',
+          'DS-80',
+          'DS-100',
+          'DS-200',
+          'DS-300',
+          'DS-400',
+          'DS-500',
+          'DS-600',
+          'DS-700',
+          'DS-800',
+        ], 'DS-60'),
       },
     },
     template: `
     <div>
-        <p style="font-size: 2.5em; margin-bottom: 1em;">
-          Based on https://type-scale.com/
-        </p>
-        <div :class="variation" style="margin-bottom: 30px">
-          <strong>{{typographyClass}}</strong>
+      <p :class="variation">
+        <b>{{variation}}</b>: Die heiße Zypernsonne quälte Max und Victoria ja böse auf dem Weg bis zur Küste.
+      </p>
+      <p style="margin-top: 1em;">
+        Usage: 
+      </p>
+      <p style="font-family: 'Courier New', courier, sans; background: #f6f6f6; padding: 1em 0.7em;">
+        <b>@include font(</b>'{{ variation }}'<b>)</b>;
+      </p>
+    </div>
+    `,
+  }))
+  .add('Ready to use Typographies 🎛', () => ({
+    props: {
+      variation: {
+        default: select('Typography variation', {
+          'Minor Second': 'ty-minor-second',
+          'Minor Third': 'ty-minor-third',
+          'Major Third': 'ty-major-third',
+          'Perfect Fourth': 'ty-perfect-fourth',
+          'Augmented fourth': 'ty-augmented-fourth',
+          'Perfect Fifth': 'ty-perfect-fifth',
+          'Golden Ration': 'ty-golden-ratio',
+        },
+        'ty-minor-second'),
+      },
+      fontSize: {
+        type: Number,
+        default: number('fontSize', 16, { min: 0, max: 900 }),
+      },
+    },
+    computed: {
+      style() {
+        return `margin-bottom: 30px; font-size: ${this.fontSize}px`;
+      },
+    },
+    template: `
+    <div>
+        <div :class="variation" :style="style">
           <h1>H1. Hello, world!</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -49,10 +73,10 @@ storiesOf('Typography/Typographies', module)
             Cras quis congue nulla. Duis dolor justo, commodo eu vulputate sed, ultrices non dui. Sed vel ligula a quam laoreet lacinia. Integer porttitor, elit vel bibendum tincidunt, sem nulla tristique enim, venenatis eleifend mi ipsum eget purus. Vestibulum pulvinar massa ac ex facilisis, eget sagittis velit ultricies. Mauris in libero et dui ultrices rhoncus. Curabitur molestie elit tellus, in pulvinar sem placerat at. Maecenas porta tellus eu odio tempor faucibus. Aliquam mattis dignissim nunc vel facilisis. Vivamus sit amet lectus id nisl sollicitudin sollicitudin quis nec ex. Nulla facilisi. Vestibulum tellus enim, iaculis ut felis eget, condimentum aliquam nunc. Phasellus vehicula tincidunt dolor ac tristique.
           </p>
           <p>
-          Small text:
-          <small>
-             Cras scelerisque in sapien ullamcorper scelerisque
-          </small>
+            <b>Small text:</b>
+            <small>
+              Based on https://type-scale.com/
+            </small>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Cras quis congue nulla. Duis dolor justo, commodo eu vulputate sed, ultrices non dui. Sed vel ligula a quam laoreet lacinia. Integer porttitor, elit vel bibendum tincidunt, sem nulla tristique enim, venenatis eleifend mi ipsum eget purus. Vestibulum pulvinar massa ac ex facilisis, eget sagittis velit ultricies. Mauris in libero et dui ultrices rhoncus. Curabitur molestie elit tellus, in pulvinar sem placerat at. Maecenas porta tellus eu odio tempor faucibus. Aliquam mattis dignissim nunc vel facilisis. Vivamus sit amet lectus id nisl sollicitudin sollicitudin quis nec ex. Nulla facilisi. Vestibulum tellus enim, iaculis ut felis eget, condimentum aliquam nunc. Phasellus vehicula tincidunt dolor ac tristique.
@@ -72,5 +96,8 @@ storiesOf('Typography/Typographies', module)
           </p>
         </div>
       </div>
+    `,
+    style: `
+      color: red;
     `,
   }));
