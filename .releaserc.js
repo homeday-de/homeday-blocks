ee3b4125f077ed77544d889b1d5ceab6e569d060
ilyasmez <ilyas@amezouar.com>
2021-02-02 12:51:56 +0100

    👷  Automate the releasing/publishing process

---
+++ .releaserc.js
@@ -0,0 +1,56 @@
+const dateFormat = require('dateformat');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const { gitmojis } = require('./.semantic-release/semantic-gitmojis');
+
+const TEMPLATE_DIR = './.semantic-release/templates';
+
+const template = readFileSync(
+  join(__dirname, TEMPLATE_DIR, 'default-template.hbs'),
+).toString();
+const commitTemplate = readFileSync(
+  join(__dirname, TEMPLATE_DIR, 'commit-template.hbs'),
+).toString();
+
+module.exports = {
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+        releaseRules: {
+          major: gitmojis.filter(({ semver }) => semver === 'major').map(({ code }) => code),
+          minor: gitmojis.filter(({ semver }) => semver === 'minor').map(({ code }) => code),
+          patch: gitmojis.filter(({ semver }) => semver === 'patch').map(({ code }) => code),
+        },
+        releaseNotes: {
+          template,
+          partials: { commitTemplate },
+          helpers: {
+            datetime(format = 'UTC:yyyy-mm-dd') {
+              return dateFormat(new Date(), format);
+            },
+            groupCommits(commits, options) {
+              const allCommits = Object.values(commits).flat();
+              let groupedCommits = {};
+
+              allCommits.forEach((commit) => {
+                const gitmoji = gitmojis.find(({ emoji }) => emoji == commit.gitmoji);
+
+                groupedCommits = {
+                  ...groupedCommits,
+                  [gitmoji.semver]: [
+                    ... groupedCommits[gitmoji.semver] || [],
+                    commit,
+                  ],
+                };
+              });
+
+              return options.fn(groupedCommits);
+            },
+          },
+        },
+      },
+    ],
+    '@semantic-release/github',
+    '@semantic-release/npm',
+  ],
+};
