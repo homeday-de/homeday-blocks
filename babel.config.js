da9df25d74c0db4079e73136c4063edf5c56ce5d
Daniel González <mgogo@hey.com>
2021-06-17 21:02:06 -0500

    💚 (babel): remove warnings on for babel config

--- babel.config.js
+++ babel.config.js
@@ -6,4 +6,9 @@ module.exports = {
       },
     }],
   ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 };
