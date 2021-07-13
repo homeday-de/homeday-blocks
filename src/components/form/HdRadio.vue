b44055fe84860ceaa9a851a455ed56e03a26a696
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-07-13 17:49:35 +0200

    ✨ Add HdRadioIndicator component

--- src/components/form/HdRadio.vue
+++ src/components/form/HdRadio.vue
@@ -44,7 +44,11 @@
           :disabled="disabled"
           type="radio"
         />
-        <div class="radio__circle"></div>
+        <HdRadioIndicator
+          :checked="item.value === value"
+          :disabled="disabled"
+          :invalid="hasError"
+        />
         <label
           :for="getItemName(item)"
           class="radio__label"
@@ -61,6 +65,7 @@ import merge from 'lodash/merge';
 import { getMessages } from 'homeday-blocks/src/lang';
 import FieldBase from './FieldBase.vue';
 import formField from './formFieldMixin';
+import HdRadioIndicator from './HdRadioIndicator.vue';
 
 export default {
   name: 'HdRadio',
@@ -69,6 +74,7 @@ export default {
   ],
   components: {
     FieldBase,
+    HdRadioIndicator,
   },
   props: {
     name: {
@@ -249,30 +255,12 @@ export default {
   outline: 0;
 }
 
-.radio-wrapper--errored > .radio > .radio__circle {
-  border-color: $error-color;
-
-  &:hover {
-    transition: ease-in $time-s;
-    box-shadow: 0 0 0 7px rgba($error-color, 0.15);
-  }
-}
-
 .radio-wrapper--disabled {
   pointer-events: none;
 
   > .radio,
   > .radio--selected {
     color: getShade($neutral-gray, 70);
-
-    > .radio__circle {
-      border-color: getShade($neutral-gray, 70);
-      background-color: transparent;
-
-      &::before {
-        background-color: getShade($neutral-gray, 70);
-      }
-    }
   }
 }
 
@@ -293,66 +281,12 @@ export default {
     display: none;
   }
 
-  > .radio__circle {
-    position: relative;
-    width: 20px;
-    height: 20px;
-    max-width: 20px;
-
-    border-radius: 50%;
-    border: 2px solid $neutral-gray;
-
-    outline-width: 0;
-    overflow: hidden;
-
-    &:hover,
-    &:active,
-    &:focus {
-      transition: ease-in-out $time-s;
-      box-shadow: 0 0 0 7px getShade($secondary-color, 90);
-    }
-
-    &::before {
-      content: "";
-
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-
-      background-color: $white;
-
-      opacity: 0;
-      transform: scale(0) translate(-50%, -50%);
-      transform-origin: top left;
-      transition: transform $time-s, opacity $time-s;
-    }
-  }
-
   > .radio__label {
     flex: 1;
     padding-left: $sp-s;
     cursor: pointer;
     text-align: left;
     @include font("text-small");
-  }
-}
-
-.radio--selected > .radio__circle {
-  border-color: getShade($secondary-color, 110);
-  background-color: getShade($secondary-color, 110);
-
-  &:hover,
-  &:active,
-  &:focus {
-    box-shadow: 0 0 0 7px rgba(getShade($secondary-color, 110), 0.15);
-  }
-
-  &:before {
-    transform: scale(1) translate(-50%, -50%);
-    opacity: 1;
   }
 }
 </style>
