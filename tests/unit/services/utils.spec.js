b473764a4f3edd0962bc25068432e346b11a4513
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-07-14 09:42:07 +0200

    ✅ Add basic test for cloneVNodeElement

--- tests/unit/services/utils.spec.js
+++ tests/unit/services/utils.spec.js
@@ -1,4 +1,5 @@
-import { generateUniqueNumbers, loadScript } from '@/services/utils';
+import { generateUniqueNumbers, loadScript, cloneVNodeElement } from '@/services/utils';
+import { mount } from '@vue/test-utils';
 
 describe('Utils service', () => {
   describe('generateRandomNumbers', () => {
@@ -30,6 +31,69 @@ describe('Utils service', () => {
       expect(scriptTag.getAttribute('id')).toEqual('10');
       expect(scriptTag.parentElement.tagName).toEqual('HEAD');
       expect(scriptTag.parentElement.firstChild.getAttribute('src')).toEqual('/example');
+    });
+  });
+
+  describe('cloneVNodeElement', () => {
+    it('creates a clone of a vue dom element', () => {
+      /** @type {Vue.Component<Vue>} */
+      const HeaderComponent = ({
+        props: ['name'],
+        data: () => ({ greeting: 'Hello' }),
+        computed: {
+          computedProperty() {
+            return 2 + 2;
+          },
+        },
+        methods: {
+          sumComputedProperty() {
+            return this.computedProperty + 2;
+          },
+        },
+        template: `
+          <div>
+            <h1>{{ greeting }}, {{ name ||"comrade" }}</h1>
+            <span>The result is: {{ computedProperty }}</span>
+            <span>The sum is: {{ sumComputedProperty() }}</span>
+          </div>
+        `,
+      });
+
+      /** @type {Vue.Component<Vue>} */
+      const MyComponent = ({
+        render(h) {
+          /** @type {import('vue').VNode[]} */
+          const children = this.$slots.default || [];
+
+          /** @type {import('vue').VNode[]} */
+          const clones = children.map((vnode) => {
+            const clone = cloneVNodeElement(
+              vnode,
+              {
+                props: {
+                  name: 'Alexander',
+                },
+              },
+              h,
+            );
+            return h('div', {}, [clone]);
+          });
+
+          return h('div', {}, [
+            clones,
+            children,
+          ]);
+        },
+      });
+
+      const wrapper = mount(MyComponent, {
+        slots: {
+          default: [HeaderComponent],
+        },
+      });
+
+      expect(wrapper.text()).toContain('Alexander');
+      expect(wrapper.text()).toContain('comrade');
     });
   });
 });
