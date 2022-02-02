20f6fd6185c4fc2b7fe5f19f67dbe948b1eb2b16
Sussie Casasola <SusCasasola@users.noreply.github.com>
2022-02-02 13:30:52 +0100

    ✨ Phone input component (#860)
    * :construction: Initial component, with main structure
    
    * :construction: Finish the whole functionality of the input
    
    * :lipstick: Add styles for the dropdown button
    
    * :lipstick: Add styling for the dropdown options
    
    * :sparkles: Add the helper text and automatic focus
    
    * :wheelchair: Added keyboard navigation for select
    
    * :construction: Refactor the format of the component
    
    * :globe_with_meridians: Update country names to German
    
    * :white_check_mark: Add spec for the phone input
    
    * :bug: Render preferred countries on alphabetical order
    
    * :sparkles: Add alphabet listener
    
    * :fire: Use a library for flags and phone codes
    
    * :art: Fix PR review comments
    
    * :heavy_plus_sign: Added a library for the format of phone codes
    
    * :fire: Removed format function
    
    * :white_check_mark: Update format of tests
    
    * :adhesive_bandage: Update comments from review
    
    * :globe_with_meridians: Use i18n for labels

---
+++ src/stories/HdInputPhone.stories.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { HdInputPhone } from 'homeday-blocks';
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import FormWrapper from 'homeday-blocks/src/storiesWrappers/FormWrapper';
+
+storiesOf('Components/Form/HdInputPhone', module)
+  .addParameters({ options: { enableShortcuts: false } })
+  .addDecorator(FormWrapper)
+  .add('simple', () => ({
+    components: { HdInputPhone },
+    template: `
+      <HdInputPhone
+        v-model="value"
+        name="test"
+        lang="de"
+        :preferredCountries="['DE', 'AT', 'BE']"
+      />
+    `,
+    data() {
+      return {
+        value: '',
+      };
+    },
+    watch: {
+      value(value) {
+        action('input')(value);
+      },
+    },
+  }));
