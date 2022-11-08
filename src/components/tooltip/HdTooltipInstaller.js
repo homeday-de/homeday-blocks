1d8954058aeb3e9ab78bdbf03de727fd1ef7d7c7
Aymen Bareche <aym3nb@gmail.com>
2022-11-08 11:56:34 +0100

    :bug: Use consuming project's Vue instance instead own instance (#1087)

--- src/components/tooltip/HdTooltipInstaller.js
+++ src/components/tooltip/HdTooltipInstaller.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-new */
-import Vue from 'vue';
 import tooltipDirective from './tooltipDirective';
 import HdTooltip from './HdTooltip.vue';
 
-export default function install() {
+export default function install(Vue) {
   Vue.directive('hd-tooltip', tooltipDirective);
   const TooltipComponent = Vue.extend(HdTooltip);
   new TooltipComponent().$mount();
