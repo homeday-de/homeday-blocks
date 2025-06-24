39ae44ce2489d415a1165b149fd75614557de9f4
aym3nb <aym3nb@gmail.com>
2025-06-24 12:59:05 +0200

    🔧 Adjust Storybook main config to specify framework and enable auto docs generation

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,13 @@ const generateWebpackAlias = require('../webpack-alias');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-knobs'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
   core: {
     builder: 'webpack5',
+  },
+  framework: '@storybook/vue-webpack5',
+  docs: {
+    autodocs: true,
   },
   webpackFinal: async (config) => {
     config.resolve.alias = generateWebpackAlias({
