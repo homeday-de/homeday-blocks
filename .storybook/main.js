fca22ca450632944e08dd6f10b7d0a6ecdba314a
aym3nb <aym3nb@gmail.com>
2025-06-25 15:50:45 +0200

    🔧 Adjust Storybook main config to include MDX docs files
    & Remove deprecated core.builder in favor of the new framework flag

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const generateWebpackAlias = require('../webpack-alias');
 
-module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
-  core: {
-    builder: 'webpack5',
-  },
   framework: '@storybook/vue-webpack5',
   docs: {
     autodocs: true,
@@ -28,3 +25,5 @@ module.exports = {
     return config;
   },
 };
+
+export default config;
